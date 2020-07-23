8b0b8e20e183edec25026a801874748136ed1955
Joaquin Palma <42578792+Rodp63@users.noreply.github.com>
2020-07-23 14:32:09 -0500

    fixing ^M

--- interpolacion/lagrange.m
+++ interpolacion/lagrange.m
@@ -1,1 +1,27 @@
-function L = lagrange(x, y, x0)  n = length(x);  pl = 0;  for i=1 : n      p = 1;      for j=1:n          if i ~= j            p = conv(p, poly(x(j))) / (x(i) - x(j));            #conv -> multiplica polinomios,            #poly -> polinomio con raices com argumento          end      end      pl += y(i) * p;  end  m = polyval(pl, x0)  L = pl;    xx = min(x):0.1:max(x);  yy = polyval(pl, xx);  hold on  plot(x,y,'or','LineWidth',2,'MarkerFaceColor','r')  plot(xx,yy), grid  hold off  endfunction
+function L = lagrange(x, y, x0)
+
+  n = length(x);
+  pl = 0;
+  for i=1 : n
+      p = 1;
+      for j=1:n
+          if i ~= j
+            p = conv(p, poly(x(j))) / (x(i) - x(j));
+            #conv -> multiplica polinomios,
+            #poly -> polinomio con raices com argumento
+          end
+      end
+      pl += y(i) * p;
+  end
+  m = polyval(pl, x0)
+  L = pl;
+  
+  xx = min(x):0.1:max(x);
+  yy = polyval(pl, xx);
+  hold on
+  
+  plot(x,y,'or','LineWidth',2,'MarkerFaceColor','r')
+  plot(xx,yy), grid
+  hold off
+  
+endfunction
