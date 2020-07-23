7b8e8847ce860db485dd999b5aebc4e09c454f4d
Joaquin Palma <42578792+Rodp63@users.noreply.github.com>
2020-07-23 14:33:39 -0500

    fixing ^M

--- raices/deriv.m
+++ raices/deriv.m
@@ -1,1 +1,6 @@
-function d = deriv(x, h = 0.000001)    d = (fun(x+h) - fun(x)) / (2 * h);  fprintf('f`(x) = %.8f\n',d);  endfunction
+function d = deriv(x, h = 0.000001)
+  
+  d = (fun(x+h) - fun(x-h)) / (2 * h);
+  fprintf('f`(x) = %.8f\n',d);
+  
+endfunction
